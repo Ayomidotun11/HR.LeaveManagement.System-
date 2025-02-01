65f31d2f5c0e335160c2ab933969d1ca4c76525c
Odebode Ayomidotun <odebode.ayomidotun@gmail.com>
2025-02-01 11:40:05 +0100

    second commit on the HR Leave Management System.Implementing CQRS for leave type   Add README.md.

---
+++ HR.LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            // Explicitly specify the assembly for AutoMapper
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            // Register MediatR services
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            return services;
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.DeleteLeaveType
+{
+    public class UpdateLeaveTypeCommandHandler : IRequestHandler<UpdateLeaveTypeCommand, Unit>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public UpdateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            this._mapper = mapper;
+            this._leaveTypeRepository = leaveTypeRepository;
+        }
+        public async Task<Unit> Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            //validate incoming data 
+            var validator = new UpdateLeaveTypeCommandValidator(_leaveTypeRepository);
+            var validationResult = await validator.ValidateAsync(request);
+
+            // convert to domain entity object 
+            var leaveTypeToUpdate = _mapper.Map<Domain.LeaveType>(request);
+
+            //update 
+            await _leaveTypeRepository.UpdateAsync(leaveTypeToUpdate);
+
+            return Unit.Value;
+            
+        }
+    }
+}
